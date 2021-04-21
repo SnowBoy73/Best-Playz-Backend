5ec8cefd928f3a996ffdca6d4136d54bd88f9369
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-04-21 21:13:11 +0200

    Update comment.gateway.ts

--- src/comment/comment.gateway.ts
+++ src/comment/comment.gateway.ts
@@ -1,17 +1,33 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer,
+  WebSocketServer
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class CommentGateway {
+export class CommentGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
 
   @SubscribeMessage('comment')
-  handleEvent(@MessageBody() comment: string): string {
-    console.log('works' + comment);
-    return comment + 'boo';
+  handleEvent(@MessageBody() data: string): string {
+    console.log('works ' + data);
+    this.server.emit('comments', data);
+    return data + 'boo';
+  }
+
+  handleConnection(client: Socket, ...args: any[]): any {
+    console.log('Client Connect', client.id);
+    // client.emit('allMessages', this.chatService.getMessages());
+    // this.server.emit('clients', this.chatService.getClients());
+  }
+
+  handleDisconnect(client: Socket): any {
+    // this.chatService.delete(client.id);
+    // this.server.emit('clients', this.chatService.getClients());
+    console.log('Client Disconnect', client.id);
   }
 }
