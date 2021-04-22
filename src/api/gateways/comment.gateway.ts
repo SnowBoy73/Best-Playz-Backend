9f4e3f72430e87ac4016b4e78c4f0f76977f64f6
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-04-22 18:49:22 +0200

    early login

--- src/api/gateways/comment.gateway.ts
+++ src/api/gateways/comment.gateway.ts
@@ -11,19 +11,24 @@ import { Socket } from 'socket.io';
 @WebSocketGateway()
 export class CommentGateway
   implements OnGatewayConnection, OnGatewayDisconnect {
-  allComments: string[] = [];  // TEMP
+  allComments: string[] = []; // TEMP
   @WebSocketServer() server;
   @SubscribeMessage('comment')
-  handleEvent(@MessageBody() comment: string): string {
-    console.log('works ' + comment);
+  handleCommentEvent(@MessageBody() comment: string): string {
+    console.log('comment: ' + comment);
     this.allComments.push(comment);
     this.server.emit('newComment', comment);
     return comment + 'boo';
   }
 
+  @SubscribeMessage('login')
+  handleLoginEvent(@MessageBody() nickname: string): void {
+    console.log('login works ', nickname);
+  }
+
   handleConnection(client: Socket, ...args: any[]): any {
     console.log('Client Connect', client.id);
-     client.emit('allComments', this.allComments); // chatService.getMessages());
+    client.emit('allComments', this.allComments); // chatService.getMessages());
     // this.server.emit('clients', this.chatService.getClients());
   }
 
