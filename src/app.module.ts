f876ac321d41e8239789684509a76012edad77cd
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-04-28 21:07:09 +0200

    nestjs configand PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommentModule } from './api/comment.module';
 import { CommentService } from './core/services/comment.service';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [CommentModule],
+  imports: [
+    CommentModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [CommentService],
 })
