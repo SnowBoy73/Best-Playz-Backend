d924bb2b79fa8c1ad33e2569144b5328b6751b3e
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-05-03 14:50:05 +0200

    hs post and receive

--- src/api/gateways/leaderboard.gateway.ts
+++ src/api/gateways/leaderboard.gateway.ts
@@ -12,21 +12,15 @@ import {
   ICommentServiceProvider,
 } from '../../core/primary-ports/comment.service.interface';
 import { Socket } from 'socket.io';
-import { CommentService } from '../../core/services/comment.service';
-import { WelcomeDto } from '../dtos/welcome.dto';
 import { Inject } from '@nestjs/common';
-import { loginDto } from '../dtos/login.dto';
-import { CommentClient } from '../../core/models/comment-client.model';
-import { CommentDto } from '../dtos/comment.dto';
-import { CommentEntity } from '../../infrastructure/data-source/entities/comment.entity';
-import { CommentModel } from '../../core/models/comment.model';
 
 @WebSocketGateway()
 export class LeaderboardGateway {
   @WebSocketServer() server;
-
   @SubscribeMessage('highscore')
   handleHighscoreEvent(@MessageBody() data: string): string {
+    console.log('Leaderboard entry = ' + data);
+    this.server.emit('newHighscore', data);
     return data + ' Leaderboard';
   }
 }
